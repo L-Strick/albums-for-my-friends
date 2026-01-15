3deec3ab35f0e4c54be8ab45cc28c9b0a11fe6bb
Lukus Stricker <lukus@zagaran.com>
2026-01-15 10:10:56 -0500

    Date ordering

--- common/context_processors.py
+++ common/context_processors.py
@@ -10,4 +10,5 @@ def django_settings(request):
         "user_id": request.user.id,
         "DATATABLES_CSS_URL": '//cdn.datatables.net/2.3.6/css/dataTables.dataTables.min.css',
         "DATATABLES_JS_URL": "//cdn.datatables.net/2.3.6/js/dataTables.min.js",
+        "DATATABLES_DATE_URL": "//cdn.datatables.net/plug-ins/2.3.6/sorting/datetime-moment.js",
     }
