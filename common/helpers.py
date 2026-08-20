27f1fab511656732aabcfc476521df908385d362
Lukus Stricker <lukus@zagaran.com>
2026-08-20 16:10:33 -0400

    Fix helper

--- common/helpers.py
+++ common/helpers.py
@@ -1,3 +1,4 @@
+from common.constants import MISSED_ALBUM_CAP
 from common.models import User, Album
 
 
@@ -5,10 +6,15 @@ def get_valid_users():
     recent_albums = Album.objects.order_by('-made_todays_album').prefetch_related('reviews', 'reviews__user')[:MISSED_ALBUM_CAP]
     valid_users = set()
     # The Grant Exception 🫡
-    valid_users.add(User.objects.get(email="gbirindelli20@gmail.com").id)
+    grant = User.objects.filter(email="gbirindelli20@gmail.com")
+    if grant.exists():
+        valid_users.add(grant.id)
     for album in recent_albums:
         reviewed_users = [review.user for review in album.reviews.all()]
         for user in reviewed_users:
             valid_users.add(user.id)
+
+    if len(valid_users) == 0:
+        valid_users = User.objects.all().values_list("id", flat=True)
 
     return User.objects.filter(id__in=valid_users)
